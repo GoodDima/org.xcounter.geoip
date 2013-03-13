cb4b6ad4064d9a7350abc3c6895b89532729b731
Dmitry Gudkov <good.dima@gmail.com>
2013-03-13 17:21:20 +0200

    whereis

--- src/main/java/org/xcounter/geoip/routes/WhereisRoute.java
+++ src/main/java/org/xcounter/geoip/routes/WhereisRoute.java
@@ -1,5 +1,8 @@
 package org.xcounter.geoip.routes;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import spark.Request;
 import spark.Response;
 
@@ -13,8 +16,16 @@ public class WhereisRoute extends UIRoute{
     @Override
     public Object handle(Request arg0, Response arg1)
     {
-
         String site = arg0.params("site");
+        try
+        {
+            InetAddress.getAllByName(site);
+        } catch (UnknownHostException e)
+        {
+            halt(500);
+        }
+        
+        
         return site;
     }
 
